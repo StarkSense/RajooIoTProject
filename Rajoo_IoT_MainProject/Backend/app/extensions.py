e5ebe64eb771673130746aca53b25bfa6c3dc99d
Jadeja Hemadri - HJJ <hemadri.hjj.jadeja@ammann.com>
2026-01-22 15:34:57 +0530

    everlet logic has been removed from run.py and extensions.py

--- Rajoo_IoT_MainProject/Backend/app/extensions.py
+++ Rajoo_IoT_MainProject/Backend/app/extensions.py
@@ -1,13 +1,42 @@
-import os
+# import os
+# from flask_socketio import SocketIO
+# from flask_jwt_extended import JWTManager
+
+
+# ASYNC_MODE = os.getenv("SOCKETIO_ASYNC_MODE", "threading")
+
+# socketio = SocketIO(
+#     cors_allowed_origins="*",
+#     async_mode=ASYNC_MODE
+# )
+
+# jwt = JWTManager()
+
+
+
+
+# from flask_socketio import SocketIO
+# from flask_jwt_extended import JWTManager
+
+# socketio = SocketIO(
+#     cors_allowed_origins="*",
+#     async_mode="threading",
+#     ping_interval=25,
+#     ping_timeout=60
+# )
+
+# jwt = JWTManager()
+
+
+
 from flask_socketio import SocketIO
 from flask_jwt_extended import JWTManager
 
-
-ASYNC_MODE = os.getenv("SOCKETIO_ASYNC_MODE", "threading")
-
 socketio = SocketIO(
     cors_allowed_origins="*",
-    async_mode=ASYNC_MODE
+    async_mode="threading",  
+    ping_interval=25,
+    ping_timeout=60
 )
 
 jwt = JWTManager()
--- Rajoo_IoT_MainProject/Backend/run.py
+++ Rajoo_IoT_MainProject/Backend/run.py
@@ -21,6 +21,39 @@
 #     use_reloader=False
 # )
 
+
+
+
+# ----------------------------------------------------------------------------------------------------
+# from app import create_app
+# from app.extensions import socketio
+# from app.services.db_service import init_db
+# from app.services.dummy_data_provider import dummy_data_task
+# from app.sockets.telemetry_socket import telemetry_task
+
+# app = create_app()
+
+# if __name__ == "__main__":
+#     init_db()
+
+#     # -----------------------------
+#     # START BACKGROUND TASKS
+#     # -----------------------------
+#     socketio.start_background_task(dummy_data_task)
+#     socketio.start_background_task(telemetry_task)
+
+#     # -----------------------------
+#     # START SERVER
+#     # -----------------------------
+#     socketio.run(
+#         app,
+#         host="0.0.0.0",
+#         port=5000,
+#         debug=True,
+#         use_reloader=False
+#     )
+
+
 from app import create_app
 from app.extensions import socketio
 from app.services.db_service import init_db
@@ -32,19 +65,13 @@ app = create_app()
 if __name__ == "__main__":
     init_db()
 
-    # -----------------------------
-    # START BACKGROUND TASKS
-    # -----------------------------
     socketio.start_background_task(dummy_data_task)
     socketio.start_background_task(telemetry_task)
 
-    # -----------------------------
-    # START SERVER
-    # -----------------------------
     socketio.run(
         app,
         host="0.0.0.0",
         port=5000,
         debug=True,
-        use_reloader=False
+        use_reloader=False   
     )
